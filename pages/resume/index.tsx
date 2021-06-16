1144b0d99f1c14a8aade3db6443a47a74d2ccccc
Charles Barnes <charles@charlesabarnes.com>
2021-06-16 05:05:20 -0500

    add initial resume path

---
+++ pages/resume/index.tsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+class Resume extends Component {
+  render() {
+    return <div>
+      <h1>Testing text works!</h1>
+    </div>
+  }
+}
+
+export default Resume
